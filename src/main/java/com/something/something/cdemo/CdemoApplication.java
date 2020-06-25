1f230935e755d1285d0d9e480de9c6875341413d
szareian <shahin.zareian@gmail.com>
2020-06-25 10:01:04 +0200

    Initial commit

---
+++ src/main/java/com/something/something/cdemo/CdemoApplication.java
@@ -0,0 +1,13 @@
+package com.something.something.cdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdemoApplication.class, args);
+	}
+
+}
